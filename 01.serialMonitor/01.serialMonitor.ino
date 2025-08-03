69300244c07bba2cb8a1e743d385fc057c3847d4
Sebastian K <sebastian.king19@education.nsw.gov.au>
2025-08-04 09:39:50 +1000

    Update 01.serialMonitor.ino

--- 01.serialMonitor/01.serialMonitor.ino
+++ 01.serialMonitor/01.serialMonitor.ino
@@ -1,5 +1,5 @@
 /*
-  Author: 
+  Author: Sebastian K
   
   Learning Intention:
   The students will understand the difference between the inbuilt setup and loop methods,
@@ -21,12 +21,22 @@
 
 // The setup function runs once when you press reset or power the board
 void setup() {
-
+  // setup a serial connection and configure the communication speed
+  Serial.begin(9600);
+  // debug serial connection by printing a conformation to the serial monitor
+  Serial.print("Serial monitor is configured to 9600 baud rate");
 }
 
 // The loop function runs over and over again forever
 void loop() {
-
+  Serial.print("Pin-A0:");
+  Serial.print(analogRead(A0));
+  Serial.print(",");
+  Serial.print("Pin-A1:");
+  Serial.print(analogRead(A1));
+  Serial.print(",");
+  Serial.print("Pin-A2:");
+  Serial.println(analogRead(A2));
 }
 
 
